01ff96a6de9d324ab5cdaa0f7aee1caae5109b60
nunocourela <courela@gmail.com>
2021-04-03 12:27:56 +0100

    #applyover tests

---
+++ cypress/integration/applyover.spec.js
@@ -0,0 +1,15 @@
+import JsonTransformer from "../../jsonTransformer.js";
+
+context('Apply Over', () => {
+    beforeEach(() => {
+    });
+
+    it('input retake', () => {
+        const input = '{ "d": [ "one", "two", "three" ], "values": [ "z", "c", "n" ]}';
+        const transformer = '{ "result": "#applyover({ \'condition\': { \'#loop($.values)\': { \'test\': \'#ifcondition(#stringcontains(#valueof($.d[0]),#currentvalue()),true,yes,no)\' } } }, \'#exists($.condition[?(@.test==\'yes\')])\')", "after_result": "#valueof($.d[0])" }';
+
+        var result = new JsonTransformer({ evaluationMode: [ "strict" ]}).transform(transformer, input);
+
+        expect(result).to.deep.equal({ result: true, after_result: "one"});
+    });
+});
