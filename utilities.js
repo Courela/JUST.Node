30d36c7d145facf57072889eaee3c4c24c1a9091
Nuno Courela <nuno.courela@truphone.com>
2021-04-27 00:42:51 +0100

    Indent utilities.js

--- utilities.js
+++ utilities.js
@@ -1,125 +1,104 @@
 import functions from "./functions.js";
 
 function groupArray(arr, groupingElement, groupedElement) {
-  let groupedPair = {};
+    let groupedPair = {};
 
-  if (arr)
-  {
-    arr.forEach(eachObj => {
-        let groupToken = functions.execute('valueof', [ '$.' + groupingElement ], eachObj, null);
-
-        if (groupToken)
-        {
-            let valueOfToken = groupToken.value;
-
-            if (Object.keys(groupedPair).includes(valueOfToken))
+    if (arr) {
+        arr.forEach(eachObj => {
+            let groupToken = functions.execute('valueof', [ '$.' + groupingElement ], eachObj, null);
+            if (groupToken)
             {
-                let oldArr = groupedPair[valueOfToken];
-                let clonedObj = Object.assign({}, eachObj);
-                delete clonedObj[groupingElement];
-
-                oldArr.push(clonedObj);
-
-                groupedPair[valueOfToken] = oldArr;
+                let valueOfToken = groupToken.value;
+                if (Object.keys(groupedPair).includes(valueOfToken)) {
+                    let oldArr = groupedPair[valueOfToken];
+                    let clonedObj = Object.assign({}, eachObj);
+                    delete clonedObj[groupingElement];
+                    oldArr.push(clonedObj);
+                    groupedPair[valueOfToken] = oldArr;
+                }
+                else {
+                    let clonedObj = Object.assign({}, eachObj);
+                    delete clonedObj[groupingElement];
+                    let newArr = [ clonedObj ];
+                    groupedPair[valueOfToken] = newArr;
+                }
             }
-            else
-            {
-                let clonedObj = Object.assign({}, eachObj);
-                delete clonedObj[groupingElement];
-                let newArr = [ clonedObj ];
+        });
+    }
 
-                groupedPair[valueOfToken] = newArr;
-            }
-        }
+    let resultObj = [];
+    Object.keys(groupedPair).forEach(pair => {
+        let groupObj = { };
+        groupObj[groupingElement] = pair;
+        groupObj[groupedElement] = groupedPair[pair];
+
+        resultObj.push(groupObj);
     });
-  }
 
-  let resultObj = [];
-  Object.keys(groupedPair).forEach(pair => {
-      let groupObj = { };
-      groupObj[groupingElement] = pair;
-      groupObj[groupedElement] = groupedPair[pair];
-
-      resultObj.push(groupObj);
-  });
-
-  return resultObj;
+    return resultObj;
 }
 
 function groupArrayMultipleProperties(array, groupingPropertyNames, groupedPropertyName) {
-  let groupedPair = {};
+    let groupedPair = {};
 
-  if (array)
-  {
-    array.forEach(eachObj => {
-          let groupTokens = [];
+    if (array) {
+        array.forEach(eachObj => {
+            let groupTokens = [];
 
-          groupingPropertyNames.forEach(groupPropertyName => {
-              groupTokens.push(functions.execute('valueof', [ '$.' + groupPropertyName ], eachObj, null).value);
-          });
+            groupingPropertyNames.forEach(groupPropertyName => {
+                groupTokens.push(functions.execute('valueof', [ '$.' + groupPropertyName ], eachObj, null).value);
+            });
 
-          if (groupTokens.length > 0)
-          {
-              let key = '';
+            if (groupTokens.length > 0) {
+                let key = '';
+                groupTokens.forEach(groupToken => {
+                    let valueOfToken = groupToken;
+                    if (key === '') {
+                        key += valueOfToken;
+                    } else {
+                        key += ':' + valueOfToken;
+                    }
+                });
 
-              groupTokens.forEach(groupToken => {
-                  let valueOfToken = groupToken;
-                  if (key === '')
-                      key += valueOfToken;
-                  else
-                      key += ':' + valueOfToken;
-              });
+                if (Object.keys(groupedPair).includes(key)) {
+                    let oldArr = groupedPair[key];
+                    let clonedObj = Object.assign({}, eachObj);
 
+                    groupingPropertyNames.forEach(groupPropertyName => {
+                        delete clonedObj[groupPropertyName];
+                    });
 
-              if (Object.keys(groupedPair).includes(key))
-              {
-                  let oldArr = groupedPair[key];
-                  let clonedObj = Object.assign({}, eachObj);
+                    oldArr.push(clonedObj);
+                    groupedPair[key] = oldArr;
+                }
+                else {
+                    let clonedObj = Object.assign({}, eachObj);
+                    let newArr = [];
 
-                  groupingPropertyNames.forEach(groupPropertyName => {
-                      delete clonedObj[groupPropertyName];
-                  });
+                    groupingPropertyNames.forEach(groupPropertyName => {
+                        delete clonedObj[groupPropertyName];
+                    });
+                    newArr.push(clonedObj);
+                    groupedPair[key] = newArr;
+                }
+            }
+        });
+    }
 
-                  oldArr.push(clonedObj);
-
-                  groupedPair[key] = oldArr;
-              }
-              else
-              {
-                  let clonedObj = Object.assign({}, eachObj);
-
-                  let newArr = [];
-
-                  groupingPropertyNames.forEach(groupPropertyName => {
-                      delete clonedObj[groupPropertyName];
-                  });
-                  newArr.push(clonedObj);
-
-                  groupedPair[key] = newArr;
-              }
-          }
-      });
-  }
-
-  let resultObj = [];
-  Object.keys(groupedPair).forEach(pair => {
-      let groupObj = {};
-
-      let keys = pair.split(':');
-
-      groupingPropertyNames.forEach((groupPropertyName, i) => {
-          groupObj[groupPropertyName] = keys[i];
-      });
-
-      groupObj[groupedPropertyName] = groupedPair[pair];
-
-      resultObj.push(groupObj);
-
-  });
-  return resultObj;
+    let resultObj = [];
+    Object.keys(groupedPair).forEach(pair => {
+        let groupObj = {};
+        let keys = pair.split(':');
+        groupingPropertyNames.forEach((groupPropertyName, i) => {
+            groupObj[groupPropertyName] = keys[i];
+        });
+        groupObj[groupedPropertyName] = groupedPair[pair];
+        resultObj.push(groupObj);
+    });
+    return resultObj;
 }
 
 export default {
-  groupArray,
-  groupArrayMultipleProperties
+    groupArray,
+    groupArrayMultipleProperties
 }
