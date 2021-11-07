33d565e3548ab90297d66435a9af624ac57bb1ea
TRUPHONE\Nuno.Courela <nuno.courela@truphone.com>
2021-11-07 16:14:32 +0000

    Test for looping root

--- cypress/integration/arrayinput.spec.js
+++ cypress/integration/arrayinput.spec.js
@@ -21,4 +21,13 @@ context('Array Input', () => {
 
         expect(result).to.deep.equal({ result: [{ name: "Person 1" },{ name: "Person 3" }] });
     });
+
+    it('looping root', () => {
+        const input = '[{"Id":1,"Fields":{"Email":{"Name":"Email","FieldType":3,"Value\":"test1@test.com"}},"CreatedAt":"2021-10-07T13:40:14.813Z"},{"Id":2,"Fields":{"Email":{"Name":"Email","FieldType":3,"Value":"test2@test.com"}},"CreatedAt":"2021-10-07T13:44:24.48Z"},{"Id":3,"Fields":{"Email":{"Name":"Email","FieldType":3,"Value":"test3@test.com"}},"CreatedAt":"2021-10-07T13:45:09.417Z"}]';
+        const transformer = '{"#loop($)":{"#eval(#concat(id,#tostring(#currentvalueatpath($.Id))))":"#currentvalueatpath($.Fields[\'Email\'].Value)"}}';
+
+        var result = new JsonTransformer({ evaluationMode: 'strict' }).transform(transformer, input);
+
+        expect(result).to.deep.equal([{ id1: "test1@test.com"},{ id2: "test2@test.com"},{ id3: "test3@test.com" }]);
+    });
 });
